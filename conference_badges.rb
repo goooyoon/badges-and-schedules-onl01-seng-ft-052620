c89c1a17545a37e37bc3f6ae5c794f825f29053c
Grace Kim <goooyoon@gmail.com>
2020-05-28 06:26:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,11 @@ def batch_badge_creator(attendees)
 end
 badge_messages
 end
+
+def assign_rooms(attendees)
+ badge_messages = []
+  attendees.each do |attendees|
+    badge_messages.push("Hello, my name is #{attendees}.")
+end
+badge_messages
+end
